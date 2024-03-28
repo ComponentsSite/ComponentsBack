7b6ede77ef21c9899237c15374d8c4d034459e86
clairef <claire.fernandes.ee@gmail.com>
2024-03-28 09:41:05 +0100

    Ajout Proxy

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
